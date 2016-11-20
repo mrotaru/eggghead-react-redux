ee7ebe8ca6d5d8fa0d9a22eb493cf5e72d71d350
Mihai Rotaru <mihai.rotaru@gmx.com>
2016-11-20 19:44:24 +0000

    use real Redux, React

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-let MiniRedux = (function () {
-  let Store = {
-    dispatch: function (action) {
-      this.state = this.reducer(this.state, action)
-      for (let subscriber of this.subscribers) {
-        subscriber()
-      }
-    },
-    subscribe: function (callback) {
-      this.subscribers.push(callback)
-    },
-    getState: function () {
-      return this.state
-    }
-  }
-
-  return {
-    createStore: (_reducer) => {
-      return Object.assign(Object.create(Store), {
-        state: _reducer(),
-        reducer: _reducer,
-        subscribers: []
-      })
-    }
-  }
-})()
-
-let { createStore } = MiniRedux
-
 let counterReducer = (state = { count: 0 }, action) => {
   if (action && action.type === 'INCREMENT') return { count: state.count + 1 }
   if (action && action.type === 'DECREMENT') return { count: state.count - 1 }
   return state
 }
 
-let store = createStore(counterReducer)
+let store = Redux.createStore(counterReducer)
 
-store.subscribe(() => {
-  document.body.innerText = store.getState().count
-})
+let Counter = ({count}) => (
+  <h1>{count}</h1>
+)
+
+let render = () => (
+  ReactDOM.render(
+    Counter({ count: store.getState().count }),
+    document.getElementById('root')
+  )
+)
+
+store.subscribe(() => render())
+render()
 
 document.addEventListener('click', () => {
   store.dispatch({ type: 'INCREMENT' })
