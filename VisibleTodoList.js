6a4110a5ccd38b81c33a8ba090c55cdcc28938ef
Mihai Rotaru <mihai.rotaru@gmx.com>
2016-11-27 20:10:06 +0000

    use a component with lifecycle methods to fetch todos
    We're using `ReactRouter.withRouter` to wrap the component generated by
    `ReactRedux.connect`, so on route change, the wrapped component will
    receive the new route via props, and the `componentDidUpdate` lifecycle
    hook will run.
    
    However, we can't have hooks for function components, so we define a
    simple class. In it's `render`, we just return `TodoList`, passing it the
    props. We re-assign to the same variable this class, wrapped in
    `withRouter` and then `connect`.

--- VisibleTodoList.js
+++ VisibleTodoList.js
@@ -1,14 +1,33 @@
-const mapStatetoProps = (state, { params }) => ({
-  todos: getVisibleTodos(
-    state,
-    params.filter || 'all'
-  )
-})
+class VisibleTodoList extends React.Component {
+  componentDidMount () {
+    fetchTodos(this.props.filter).then(todos =>
+      console.log(this.props.filter, todos)
+    )
+  }
+  componentDidUpdate (prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(todos =>
+        console.log(this.props.filter, todos)
+      )
+    }
+  }
+  render () {
+    return <TodoList {...this.props} />
+  }
+}
+
+const mapStatetoProps = (state, { params }) => {
+  const filter = params.filter || 'all'
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter
+  }
+}
 
 const { connect } = ReactRedux
 const { withRouter } = ReactRouter
 
-const VisibleTodoList = withRouter(connect(
+VisibleTodoList = withRouter(connect(
   mapStatetoProps,
   { onTodoClick: toggleTodo }
-)(TodoList))
+)(VisibleTodoList))
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const store = configureStore()
 
-fetchTodos('all').then(todos => console.log(todos))
-
 let render = () => {
   return ReactDOM.render(
     <Root store={store} />,
