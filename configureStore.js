11c61af3690f6881b36bcac999f1b041fbb05750
Mihai Rotaru <mihai.rotaru@gmx.com>
2016-11-27 19:37:26 +0000

    monkey patch store.dispatch to include debug info

--- configureStore.js
+++ configureStore.js
@@ -1,6 +1,26 @@
+const addLoggingToDispatch = (store) => {
+  const originalDispatch = store.dispatch
+
+  if (!console.group) {
+    return originalDispatch
+  }
+
+  return (action) => {
+    console.group(action.type)
+    console.log('%c prev state', 'color: gray', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const retVal = originalDispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return retVal
+  }
+}
+
 const configureStore = () => {
   const persistedState = loadState()
   const store = Redux.createStore(todoApp, persistedState)
+
+  store.dispatch = addLoggingToDispatch(store)
 
   store.subscribe(lodash.throttle(() => {
     saveState({
