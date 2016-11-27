a050cef8ddef848322c78dae1b0445a71a0f00fb
Mihai Rotaru <mihai.rotaru@gmx.com>
2016-11-27 20:01:20 +0000

    fake API

---
+++ fakeApi.js
@@ -0,0 +1,29 @@
+const fakeDatabase = {
+  todos: [{
+    id: uuid.v4(),
+    text: 'todo 1',
+    completed: true
+  }, {
+    id: uuid.v4(),
+    text: 'todo 2',
+    completed: true
+  }, {
+    id: uuid.v4(),
+    text: 'todo 3',
+    completed: false
+  }]
+}
+
+const delay = (ms) =>
+  new Promise(resolve => setTimeout(resolve, ms))
+
+const fetchTodos = (filter) => {
+  return delay(500).then(() => {
+    switch (filter) {
+      case 'all': return fakeDatabase.todos
+      case 'active': return fakeDatabase.todos.filter((t) => t.completed !== true)
+      case 'completed': return fakeDatabase.todos.filter((t) => t.completed === true)
+      default: throw new Error(`Unknown filter: ${filter}`)
+    }
+  })
+}
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const store = configureStore()
 
+fetchTodos('all').then(todos => console.log(todos))
+
 let render = () => {
   return ReactDOM.render(
     <Root store={store} />,
